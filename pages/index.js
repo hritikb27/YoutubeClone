036a5db12b9698233540e844db8e9361cc653042
unknown <hritikb90@gmail.com>
2022-07-20 19:33:30 +0530

    initial project setup

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className="w-[600px] h-[600px] bg-red-500">
+      
+    </div>
+  )
+}
