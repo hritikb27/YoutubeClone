136a402b37406fc72feeb6b28e80012a887bb707
unknown <hritikb90@gmail.com>
2022-07-22 18:47:25 +0530

    added id to items

--- components/Layout/CategoriesScrollableList.js
+++ components/Layout/CategoriesScrollableList.js
@@ -8,33 +8,33 @@ import "swiper/css/pagination";
 import { Navigation, Mousewheel, Keyboard } from "swiper";
 
 const items = [
-  { name: 'All', href: '#', selected: true },
-  { name: 'Music', href: '#', selected: false },
-  { name: 'Live', href: '#', selected: false },
-  { name: 'Lofi', href: '#', selected: false },
-  { name: 'Free', href: '#', selected: false },
-  { name: 'Gaming', href: '#', selected: false },
-  { name: 'Ghibli', href: '#', selected: false },
-  { name: 'Computer', href: '#', selected: false },
-  { name: 'Ambient', href: '#', selected: false },
-  { name: 'Linux', href: '#', selected: false },
-  { name: 'Classical', href: '#', selected: false },
-  { name: 'Bollywood', href: '#', selected: false },
-  { name: 'Probability', href: '#', selected: false },
-  { name: 'Calculus', href: '#', selected: false },
-  { name: 'files', href: '#', selected: false },
-  { name: 'Conversation', href: '#', selected: false },
-  { name: 'Recent', href: '#', selected: false },
-  { name: 'Watched', href: '#', selected: false },
-  { name: 'New to you', href: '#', selected: false },
-  { name: 'Ghibli', href: '#', selected: false },
-  { name: 'Computer', href: '#', selected: false },
-  { name: 'Ambient', href: '#', selected: false },
-  { name: 'Linux', href: '#', selected: false },
-  { name: 'Classical', href: '#', selected: false },
-  { name: 'Bollywood', href: '#', selected: false },
-  { name: 'Probability', href: '#', selected: false },
-  { name: 'Calculus', href: '#', selected: false },
+  { name: 'All', href: '#', selected: true , id: 1},
+  { name: 'Music', href: '#', selected: false, id: 2},
+  { name: 'Live', href: '#', selected: false, id: 3},
+  { name: 'Lofi', href: '#', selected: false, id: 4},
+  { name: 'Free', href: '#', selected: false, id: 5},
+  { name: 'Gaming', href: '#', selected: false, id: 6},
+  { name: 'Ghibli', href: '#', selected: false, id: 7},
+  { name: 'Computer', href: '#', selected: false, id: 8},
+  { name: 'Ambient', href: '#', selected: false, id: 9},
+  { name: 'Linux', href: '#', selected: false, id: 10},
+  { name: 'Classical', href: '#', selected: false, id: 11},
+  { name: 'Bollywood', href: '#', selected: false, id: 12},
+  { name: 'Probability', href: '#', selected: false, id: 13},
+  { name: 'Calculus', href: '#', selected: false, id: 14},
+  { name: 'files', href: '#', selected: false, id: 15},
+  { name: 'Conversation', href: '#', selected: false, id: 16},
+  { name: 'Recent', href: '#', selected: false, id: 17},
+  { name: 'Watched', href: '#', selected: false, id: 18},
+  { name: 'New to you', href: '#', selected: false, id: 19},
+  { name: 'Ghibli', href: '#', selected: false, id: 20},
+  { name: 'Computer', href: '#', selected: false, id: 21},
+  { name: 'Ambient', href: '#', selected: false, id: 22},
+  { name: 'Linux', href: '#', selected: false, id: 23},
+  { name: 'Classical', href: '#', selected: false, id: 24},
+  { name: 'Bollywood', href: '#', selected: false, id: 25},
+  { name: 'Probability', href: '#', selected: false, id: 26},
+  { name: 'Calculus', href: '#', selected: false, id: 27},
 ]
 
 function classNames(...classes) {
@@ -46,7 +46,7 @@ const CategoriesScrollableList = () => {
 
   const handleSelection = (item)=> {
     const newArr = itemArr.map(i=>{
-      if(i.name===item.name)return {...i, selected: true}
+      if(i.id===item.id)return {...i, selected: true}
       else return {...i, selected: false};
     })
     setItemArr(newArr)
@@ -60,7 +60,7 @@ const CategoriesScrollableList = () => {
           mousewheel={true}
           keyboard={true}
           modules={[Navigation, Mousewheel, Keyboard]}
-          slidesPerView={9} slidesPerColumn={5} spaceBetween={30}
+          slidesPerView={9} slidesPerColumn={5} spaceBetween={20}
           
           className="p-10">
           {itemArr.map((item, i) => {
